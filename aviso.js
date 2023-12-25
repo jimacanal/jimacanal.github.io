bdd534f95c393ce2e5444c184223a0aef0bd653f
jimacanal <154744479+jimacanal@users.noreply.github.com>
2023-12-25 12:33:13 +0100

    Update aviso.js

--- aviso.js
+++ aviso.js
@@ -1,13 +1,6 @@
 
-    // Verificar si la cookie 'aviso_aceptado' está presente
-    if (!document.cookie.includes('aviso_aceptado=true')) {
-        document.getElementById('aviso-legal').style.display = 'block';
-    }
-
-    // Función para aceptar el aviso y establecer la cookie
-    function aceptarAviso() {
-        document.getElementById('aviso-legal').style.display = 'none';
-        // Establecer una cookie que indica que el aviso ha sido aceptado
-        document.cookie = 'aviso_aceptado=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
-    }
+// Contenido de script.js
+function mostrarAlerta() {
+    alert("¡Hola! Esto es una alerta en JavaScript desde un archivo separado.");
+}
 
